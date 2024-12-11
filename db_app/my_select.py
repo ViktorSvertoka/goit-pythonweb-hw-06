cb612a747f8d6653d124877c1478c439a00e21ea
Viktor Svertoka <victor.svertoka@gmail.com>
2024-12-11 14:04:29 +0200

    Update my_select.py

--- db_app/my_select.py
+++ db_app/my_select.py
@@ -1,171 +1,213 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, func, desc
-from models import Student, Grade, Subject, Group, Teacher
-from colorama import Fore, Style
+from db import session
+from pprint import pprint
+from sqlalchemy import func, desc
+from colorama import Fore, Style, init
 
+from models import Student, Group, Subject, Teacher, Grade
 
-engine = create_engine(
-    "postgresql+psycopg2://postgres:qwerty123@localhost:5432/postgres"
+init(autoreset=True)
+
+# 1. Знайти 5 студентів з найвищим середнім балом по всіх предметах
+avg_grades = (
+    session.query(Grade.student_id, func.avg(Grade.grade).label("average_grade"))
+    .group_by(Grade.student_id)
+    .subquery()
 )
-Session = sessionmaker(bind=engine)
+
+five_top_students = (
+    session.query(Student.student_name, avg_grades.c.average_grade)
+    .join(avg_grades, Student.id == avg_grades.c.student_id)
+    .order_by(desc(avg_grades.c.average_grade))
+    .limit(5)
+    .all()
+)
+
+print(Fore.GREEN + "1. Результат select_1")
+pprint(five_top_students)
 
 
-def select_1():
-    """Знайти 5 студентів із найбільшим середнім балом з усіх предметів."""
-    with Session() as session:
-        students = (
-            session.query(Student.name, func.avg(Grade.value).label("avg_grade"))
-            .join(Grade, Grade.student_id == Student.id)
-            .group_by(Student.id, Student.name)
-            .order_by(desc("avg_grade"))
-            .limit(5)
-            .all()
-        )
-        return students if students else "Немає даних."
+# 2. Знайти студента з найвищим середнім балом по певному предмету
+subject_id = 3
+best_student = (
+    session.query(
+        Student.student_name,
+        func.avg(Grade.grade).label("average_grade"),
+        Subject.subject_name.label("subject_name"),
+    )
+    .join(Grade, Grade.student_id == Student.id)
+    .join(Subject, Subject.id == Grade.subject_id)
+    .filter(Grade.subject_id == subject_id)
+    .group_by(Student.id, Subject.subject_name)
+    .order_by(desc(func.avg(Grade.grade)))
+    .first()
+)
+
+if best_student:
+    print(Fore.CYAN + "2. Результат select_2 (Тема: Algorithms)")
+    pprint(
+        [
+            (
+                best_student.student_name,
+                float(best_student.average_grade),
+                best_student.subject_name,
+            )
+        ]
+    )
+else:
+    print(Fore.RED + "Немає результатів" + Style.RESET_ALL)
 
 
-def select_2(subject_id):
-    """Знайти студента із найвищим середнім балом з певного предмета."""
-    with Session() as session:
-        student = (
-            session.query(Student.name, func.avg(Grade.value).label("avg_grade"))
-            .join(Grade, Grade.student_id == Student.id)
-            .filter(Grade.subject_id == subject_id)
-            .group_by(Student.id, Student.name)
-            .order_by(desc("avg_grade"))
-            .first()
-        )
-        return student if student else f"Немає студента для предмета з id {subject_id}."
+# 3. Знайти середній бал по групах для певного предмету
+group_avrg = (
+    session.query(
+        Group.group_name.label("group_name"),
+        func.avg(Grade.grade).label("average_grade"),
+        Subject.subject_name.label("subject_name"),
+    )
+    .join(Student, Student.group_id == Group.id)
+    .join(Grade, Grade.student_id == Student.id)
+    .join(Subject, Subject.id == Grade.subject_id)
+    .filter(Grade.subject_id == subject_id)
+    .group_by(Group.id, Subject.subject_name)
+    .order_by(Group.group_name)
+    .all()
+)
+
+results = [
+    (group_name, float(average_grade), subject_name)
+    for group_name, average_grade, subject_name in group_avrg
+]
+
+print(Fore.YELLOW + "3. Результат select_3 (Тема: Cybersecurity)")
+pprint(results)
 
 
-def select_3(subject_id):
-    """Знайти середній бал у групах з певного предмета."""
-    with Session() as session:
-        groups_avg = (
-            session.query(Group.name, func.avg(Grade.value).label("avg_grade"))
-            .join(Student, Student.group_id == Group.id)
-            .join(Grade, Grade.student_id == Student.id)
-            .filter(Grade.subject_id == subject_id)
-            .group_by(Group.id, Group.name)
-            .all()
-        )
-        return (
-            groups_avg if groups_avg else f"Немає даних для предмета з id {subject_id}."
-        )
+# 4. Знайти середній бал по всіх предметах
+avrg_grade = session.query(func.avg(Grade.grade)).scalar()
+
+print(Fore.MAGENTA + "4. Результат select_4")
+pprint([float(avrg_grade)])
 
 
-def select_4():
-    """Знайти середній бал на потоці (по всій таблиці оцінок)."""
-    with Session() as session:
-        avg_grade = session.query(func.avg(Grade.value)).scalar()
-        return avg_grade if avg_grade else "Немає даних."
+# 5. Знайти, які курси викладає певний викладач
+teacher_id = 2
+courses = (
+    session.query(
+        Teacher.teacher_name.label("teacher_name"),
+        Subject.subject_name.label("subject_name"),
+    )
+    .join(Subject, Subject.teacher_id == Teacher.id)
+    .filter(Teacher.id == teacher_id)
+    .order_by(Subject.subject_name)
+    .all()
+)
+
+print(Fore.BLUE + "5. Результат select_5 (Викладач: 2)")
+pprint(courses)
 
 
-def select_5(teacher_id):
-    """Знайти які курси читає певний викладач."""
-    with Session() as session:
-        courses = (
-            session.query(Subject.name).filter(Subject.teacher_id == teacher_id).all()
-        )
-        return courses if courses else f"Немає курсів для викладача з id {teacher_id}."
+# 6. Знайти список студентів у конкретній групі
+group_id = 3
+students = (
+    session.query(
+        Group.group_name.label("group_name"),
+        Student.student_name.label("student_name"),
+    )
+    .join(Student, Student.group_id == Group.id)
+    .filter(Group.id == group_id)
+    .order_by(Student.student_name)
+    .all()
+)
+
+print(Fore.GREEN + "6. Результат select_6 (Група: Group-C)")
+pprint(students)
 
 
-def select_6(group_id):
-    """Знайти список студентів у певній групі."""
-    with Session() as session:
-        students = (
-            session.query(Student.name).filter(Student.group_id == group_id).all()
-        )
-        return students if students else f"Немає студентів в групі з id {group_id}."
+# 7. Знайти оцінки студентів у конкретній групі по конкретному предмету
+subject_id = 1
+student_grades = (
+    session.query(
+        Group.group_name.label("group_name"),
+        Student.student_name.label("student_name"),
+        Subject.subject_name.label("subject_name"),
+        Grade.grade,
+        Grade.date_of.label("date"),
+    )
+    .join(Student, Student.group_id == Group.id)
+    .join(Grade, Grade.student_id == Student.id)
+    .join(Subject, Subject.id == Grade.subject_id)
+    .filter(Group.id == group_id, Subject.id == subject_id)
+    .order_by(Student.student_name)
+    .all()
+)
+
+print(Fore.RED + "7. Результат select_7 (Група: Group-C, Тема: Software Engineering)")
+pprint(student_grades)
 
 
-def select_7(group_id, subject_id):
-    """Знайти оцінки студентів у окремій групі з певного предмета."""
-    with Session() as session:
-        grades = (
-            session.query(Student.name, Grade.value)
-            .join(Grade, Grade.student_id == Student.id)
-            .filter(Student.group_id == group_id, Grade.subject_id == subject_id)
-            .all()
-        )
-        return (
-            grades
-            if grades
-            else f"Немає оцінок для групи з id {group_id} та предмета з id {subject_id}."
-        )
+# 8. Знайти середній бал, який дає конкретний викладач на своїх предметах
+teacher_id = 7
+avrg_grades = (
+    session.query(
+        Teacher.teacher_name.label("teacher_name"),
+        Subject.subject_name.label("subject_name"),
+        func.avg(Grade.grade).label("average_grade"),
+    )
+    .join(Subject, Subject.teacher_id == Teacher.id)
+    .join(Grade, Grade.subject_id == Subject.id)
+    .filter(Teacher.id == teacher_id)
+    .group_by(Subject.id, Teacher.teacher_name)
+    .order_by(Subject.subject_name)
+    .all()
+)
+
+results = [
+    (teacher_name, subject_name, float(average_grade))
+    for teacher_name, subject_name, average_grade in avrg_grades
+]
+
+print(Fore.CYAN + "8. Результат select_8 (Викладач: Raj Koothrappali)")
+pprint(results)
 
 
-def select_8(teacher_id):
-    """Знайти середній бал, який ставить певний викладач зі своїх предметів."""
-    with Session() as session:
-        avg_grade = (
-            session.query(func.avg(Grade.value).label("avg_grade"))
-            .join(Subject, Subject.id == Grade.subject_id)
-            .filter(Subject.teacher_id == teacher_id)
-            .scalar()
-        )
-        return (
-            avg_grade if avg_grade else f"Немає оцінок для викладача з id {teacher_id}."
-        )
+# 9. Знайти список курсів, які проходить конкретний студент
+student_id = 4
+courses = (
+    session.query(
+        Student.student_name.label("student_name"),
+        Subject.subject_name.label("subject_name"),
+    )
+    .join(Grade, Grade.student_id == Student.id)
+    .join(Subject, Subject.id == Grade.subject_id)
+    .filter(Student.id == student_id)
+    .distinct()
+    .order_by(Subject.subject_name)
+    .all()
+)
+
+print(Fore.YELLOW + "9. Результат select_9 (Студент: Sheldon Cooper)")
+pprint(courses)
 
 
-def select_9(student_id):
-    """Знайти список курсів, які відвідує певний студент."""
-    with Session() as session:
-        courses = (
-            session.query(Subject.name)
-            .join(Grade, Grade.subject_id == Subject.id)
-            .filter(Grade.student_id == student_id)
-            .distinct()
-            .all()
-        )
-        return courses if courses else f"Студент з id {student_id} не відвідує курси."
+# 10. Знайти список курсів, які викладає певний викладач для конкретного студента
+teacher_id = 4
+teacher_courses = (
+    session.query(
+        Teacher.teacher_name.label("teacher_name"),
+        Student.student_name.label("student_name"),
+        Subject.subject_name.label("subject_name"),
+    )
+    .join(Subject, Subject.teacher_id == Teacher.id)
+    .join(Grade, Grade.subject_id == Subject.id)
+    .join(Student, Student.id == Grade.student_id)
+    .filter(Teacher.id == teacher_id, Student.id == student_id)
+    .distinct()
+    .order_by(Subject.subject_name)
+    .all()
+)
 
-
-def select_10(student_id, teacher_id):
-    """Список курсів, які певному студенту читає певний викладач."""
-    with Session() as session:
-        courses = (
-            session.query(Subject.name)
-            .join(Grade, Grade.subject_id == Subject.id)
-            .filter(Grade.student_id == student_id, Subject.teacher_id == teacher_id)
-            .distinct()
-            .all()
-        )
-        return (
-            courses
-            if courses
-            else f"Студент з id {student_id} не відвідує курси викладача з id {teacher_id}."
-        )
-
-
-if __name__ == "__main__":
-    print(Fore.GREEN + "Топ 5 студентів із найбільшим середнім балом:")
-    print(select_1())
-
-    print(Fore.YELLOW + "Студент з найвищим середнім балом з предмета:")
-    print(select_2(subject_id=1))
-
-    print(Fore.BLUE + "Середній бал у групах для предмета:")
-    print(select_3(subject_id=1))
-
-    print(Fore.CYAN + "Середній бал на потоці:")
-    print(select_4())
-
-    print(Fore.MAGENTA + "Курси, які читає викладач:")
-    print(select_5(teacher_id=1))
-
-    print(Fore.RED + "Студенти у групі:")
-    print(select_6(group_id=1))
-
-    print(Fore.GREEN + "Оцінки студентів у групі для предмета:")
-    print(select_7(group_id=1, subject_id=1))
-
-    print(Fore.YELLOW + "Середній бал викладача:")
-    print(select_8(teacher_id=1))
-
-    print(Fore.BLUE + "Курси, які відвідує студент:")
-    print(select_9(student_id=1))
-
-    print(Fore.CYAN + "Курси, які викладає викладач певному студенту:")
-    print(select_10(student_id=1, teacher_id=1))
+print(
+    Fore.MAGENTA
+    + "10. Результат select_10 (Порівняння: Leonard Hofstadter і Howard Wolowitz)"
+)
+pprint(teacher_courses)
